31fe3b31df2ffaaee23784ae72b93c118b4b416f
Vinicius Vicentini <viniciusvicentini@live.com>
2022-01-22 17:59:11 -0300

    chore: removing unused datas

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,25 +1,12 @@
-import { buttons } from './components/socialMediaButtons'
-import { cards } from './components/cards'
-import { result } from './components/result'
-
-import { authentication } from './templates/authentication'
-import { newsletter } from './templates/newsletter'
-
-import { contact } from './templates/contact'
-
 import { Category } from './types'
+
+import { cards } from './components/cards'
 
 export const data: Category[] = [
   {
-    name: 'Forms',
-    id: 'forms',
-    subLabel: 'User Login & Newsletter Forms',
-    children: [authentication, newsletter, contact]
-  },
-  {
-    name: 'Components',
+    name: 'Cards',
     id: 'components',
-    subLabel: 'Smaller buildings blocks like Cards, Buttons, Result ...',
-    children: [cards, buttons, result]
+    subLabel: 'User Login & Newsletter Forms',
+    children: [cards]
   }
 ]
